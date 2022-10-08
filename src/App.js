1f80a8d7cd120eb6d8b5f2b43bf877b2cffe2d0a
afrojani <sultana.ani2019@gmail.com>
2022-10-08 01:20:51 -0500

    pricing option with tailwind

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import './App.css';
 import NavBar from './components/NavBar/NavBar';
+import Pricing from './components/Pricing/Pricing';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <NavBar></NavBar>
       <h1 className='text-orange-400 text-5xl font-bold '>this is header</h1>
       <p>this is a paragraph</p>
+      <Pricing></Pricing>
     </div>
   );
 }
